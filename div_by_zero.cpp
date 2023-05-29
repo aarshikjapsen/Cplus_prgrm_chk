e4675c9032d99bfbe1d2786ef6ed66e746f34590
aarshikjapsen <134580128+aarshikjapsen@users.noreply.github.com>
2023-05-29 15:48:06 +0530

    Create div_by_zero.cpp

---
+++ div_by_zero.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int numerator = 10;
+    int denominator = 0;
+    int result;
+
+    result = numerator / denominator;
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
